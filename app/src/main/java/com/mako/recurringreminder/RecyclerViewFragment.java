5d4352fd9d6f569faa53e5fea4de3e6a008b3b17
Michał Stadryniak <stadryniak.michal@gmail.com>
2020-04-23 23:22:11 +0200

    deleted redundant parameters

--- app/src/main/java/com/mako/recurringreminder/RecyclerViewFragment.java
+++ app/src/main/java/com/mako/recurringreminder/RecyclerViewFragment.java
@@ -15,12 +15,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RecyclerViewFragment extends Fragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
 
     public RecyclerViewFragment() {
         // Required empty public constructor
@@ -30,26 +24,15 @@ public class RecyclerViewFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment RecyclerViewFragment.
      */
-    private static RecyclerViewFragment newInstance(String param1, String param2) {
-        RecyclerViewFragment fragment = new RecyclerViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private static RecyclerViewFragment newInstance() {
+        return new RecyclerViewFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
